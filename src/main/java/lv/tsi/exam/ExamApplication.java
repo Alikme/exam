fa541ff528387dfed76e01aa9283e5bf06b33a16
Jurijs Leonovičs <st61065@TTI.LV>
2019-05-24 13:10:21 +0300

    ExamST61065

---
+++ src/main/java/lv/tsi/exam/ExamApplication.java
@@ -0,0 +1,14 @@
+package lv.tsi.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+    }
+
+}
+//1 variant
