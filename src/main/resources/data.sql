d31a6c0387ac026f84ebc31968522c8b4fca07ae
Romualds Beļavskis <st60898@TTI.LV>
2019-05-24 11:55:17 +0300

    exam

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO MOVIE (ID, CATEGORY, NAME, RELEASE_YEAR) VALUES (1, 'Detective', 'Pokémon Detective Pikachu', 2019);
+INSERT INTO MOVIE (ID, CATEGORY, NAME, RELEASE_YEAR) VALUES (2, 'Fantasy', 'The Lord of the Rings: The Fellowship of the Ring', 2001);
+INSERT INTO MOVIE (ID, CATEGORY, NAME, RELEASE_YEAR) VALUES (3, 'Comedy', 'The Disaster Artist', 1980);
+INSERT INTO MOVIE (ID, CATEGORY, NAME, RELEASE_YEAR) VALUES (4, 'Horror', 'Pet semetary', 2019);
--- src/test/java/lv/tsi/exam/ExamApplicationTests.java
+++ src/test/java/lv/tsi/exam/ExamApplicationTests.java
@@ -2,15 +2,41 @@ package lv.tsi.exam;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ExamApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void contextLoads() {
+    }
+
+
+    @Test
+    public void testGetId() throws Exception{
+        mockMvc.perform(get("/movies/getId/1")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAll() throws Exception{
+        mockMvc.perform(get("/movies/getAll")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testCategory() throws Exception{
+        mockMvc.perform(get("/movies/getCat/Comedy")).andExpect(status().isOk());
     }
 
 }
